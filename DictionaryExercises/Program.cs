6205d12d03ffe605024cce49cbc72d9c7e72339f
Brigid <brigidbook@gmail.com>
2022-08-08 19:27:28 -0500

    Did the assignments

--- DictionaryExercises/Program.cs
+++ DictionaryExercises/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace DictionaryExercises
 {
@@ -6,7 +7,27 @@ namespace DictionaryExercises
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Dictionary<string, int> students = new Dictionary<string, int>();
+            while (true)
+            {
+                Console.WriteLine("Enter student name: ");
+                string studentName = Console.ReadLine();
+                if (studentName == "")
+                {
+                    Console.WriteLine("input ended.");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Enter student id: ");
+                    int studentId = int.Parse(Console.ReadLine());
+                    students.Add(studentName, studentId);
+                }
+            }
+            foreach (string studentName in students.Keys)
+            {
+                Console.WriteLine($"{studentName}: {students[studentName]}");
+            }
         }
     }
 }
--- ListExercises/Program.cs
+++ ListExercises/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace ListExercises
 {
@@ -7,6 +8,34 @@ namespace ListExercises
         static void Main(string[] args)
         {
             Console.WriteLine("Hello Lists!");
+            Console.WriteLine(FindSum(new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 }));
+            List<string> wordsList = new List<string> { "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten" };
+            printFive(wordsList);
+        }
+        static int FindSum(List<int> nums)
+        {
+            int sum = 0;
+            foreach (int x in nums)
+            {
+                if (x % 2 == 0)
+                {
+                    sum += x;
+                }
+            }
+            return sum;
+        }
+        static void printFive(List<string> words)
+        {
+            Console.Write("Enter a number: ");
+            int numOfLetters = int.Parse(Console.ReadLine());
+            foreach (string word in words)
+            {
+                if (word.Length == numOfLetters)
+                {
+                    System.Console.WriteLine(word);
+                }
+
+            }
         }
     }
 }
